83f4c5948f330caaaed73f64ccf97a538025efe7
Thomas <cnpowerman23@gmail.com>
2020-10-21 16:24:32 +1100

    Add test CollectionManager, implement in sandbox
    CollectionManager_Test is a collection manager that will fade in each
    uiElement, and pring a debug log when all items are collected.

---
+++ project/Assets/Scripts/Collection Scripts/Collection Managers/CollectionManager_Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionManager_Test : CollectionManagerBase
+{
+    public float fadeTime = 1f;
+
+
+    protected override void AllCollected()
+    {
+        //temp for debugging
+        Debug.Log("All items collected");
+    }
+
+    protected override void NewCollection(int itemID)
+    {
+        //fade the element into color
+        StartCoroutine(FadeIn(itemID, fadeTime));
+    }
+
+
+    private IEnumerator FadeIn(int itemID, float time)
+    {
+        //used to set color
+        float num = 0;
+        float scale = (1f / time);
+
+
+        while (num != 1f)
+        {
+            //increment by time, scaled
+            num += Time.deltaTime * scale;
+
+            //change color
+            uiElements[itemID].color = new Color(num, num, num);
+
+            yield return null;
+        }
+    }
+}
