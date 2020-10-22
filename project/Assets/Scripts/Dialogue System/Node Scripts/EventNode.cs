ee9e339eaa4d5ca331515bdcd2312e04cc64a9fa
Thomas <cnpowerman23@gmail.com>
2020-10-22 11:50:56 +1100

    Add EventNode
    EventNode is a node for the DialogueGraph that uses a UnityEvent, being
    invoked on activation.

---
+++ project/Assets/Scripts/Dialogue System/Node Scripts/EventNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventNode : BaseNode
+{
+    [Tooltip("When this node is activated, this event will be invoked")]
+    public UnityEvent Event;
+
+
+    public override void Activate()
+    {
+        Event.Invoke();
+    }
+}
