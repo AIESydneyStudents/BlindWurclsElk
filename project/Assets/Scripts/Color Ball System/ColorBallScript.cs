966c5f4aaf903c4361082affcad2d50aee8cef27
Thomas <cnpowerman23@gmail.com>
2020-11-04 12:55:01 +1100

    Implement paining on color ball hit
    When a color ball collides with an object with a PaintableScript, it
    will put its splat texture on the object with the selected color.
    
    For an object to be paintable, it needs to have a PaintableScript and a
    material with DecalShader. The size of the decal is dependant on the
    PaintableScript's 'textureSize', with a larger value providing a smaller
    decal (as it is more compact).

--- project/Assets/Scripts/Color Ball System/ColorBallScript.cs
+++ project/Assets/Scripts/Color Ball System/ColorBallScript.cs
@@ -8,6 +8,9 @@ public class ColorBallScript : MonoBehaviour
     // Particle effect prefab
     public GameObject particleEffect;
 
+    public Texture2D splat;
+
+    public Color color;
 
 
 
@@ -20,9 +23,20 @@ public class ColorBallScript : MonoBehaviour
             GameObject effect = Instantiate(particleEffect, transform.position, Quaternion.Euler(collision.GetContact(0).normal));
             effect.GetComponent<ParticleSystem>().Play();
         }
-        
+
 
         //paint surf
+        if (Physics.Raycast(transform.position, -collision.GetContact(0).normal, out RaycastHit hit))
+        {
+            if (hit.collider is MeshCollider)
+            {
+                PaintableScript script = hit.collider.gameObject.GetComponent<PaintableScript>();
+                if (script != null)
+                {
+                    script.PaintOn(hit.textureCoord2, splat, color);
+                }
+            }
+        }
 
 
         // Destroy the projectile
--- project/Assets/Scripts/Color Ball System/PaintableScript.cs
+++ project/Assets/Scripts/Color Ball System/PaintableScript.cs
@@ -8,7 +8,7 @@ public class PaintableScript : MonoBehaviour
     [Range(16, 8182)]
     public int textureSize = 64;
 
-    private readonly Color c_color = new Color(0, 0, 0, 0);
+    private readonly Color baseColor = new Color(0, 0, 0, 0);
 
     private Material material;
     private Texture2D texture;
@@ -19,7 +19,7 @@ public class PaintableScript : MonoBehaviour
     {
         Renderer renderer = GetComponent<Renderer>();
         if (null == renderer)
-            return;
+        { return; }
 
         foreach (Material material in renderer.materials)
         {
@@ -30,12 +30,17 @@ public class PaintableScript : MonoBehaviour
             }
         }
 
-        if (null != material)
+        if (material != null)
         {
             texture = new Texture2D(textureSize, textureSize);
-            for (int x = 0; x < textureSize; ++x)
-                for (int y = 0; y < textureSize; ++y)
-                    texture.SetPixel(x, y, c_color);
+
+            for (int x = 0; x < textureSize; x++)
+            {
+                for (int y = 0; y < textureSize; y++)
+                {
+                    texture.SetPixel(x, y, baseColor);
+                }
+            }
             texture.Apply();
 
             material.SetTexture("_DrawingTex", texture);
@@ -43,62 +48,26 @@ public class PaintableScript : MonoBehaviour
         }
     }
 
-    public void PaintOnColored(Vector2 textureCoord, Texture2D splashTexture, Color color)
-    {
-        MyPaintOn(textureCoord, splashTexture, color);
-    }
 
-    private void MyPaintOn(Vector2 textureCoord, Texture2D splashTexture, Color targetColor)
+    public void PaintOn(Vector2 textureCoord, Texture2D splashTexture, Color color)
     {
         if (isEnabled)
         {
-            int reqnx = splashTexture.width;
-            int reqny = splashTexture.height;
-            int reqX = (int)(textureCoord.x * textureSize) - (reqnx / 2);
-            int reqY = (int)(textureCoord.y * textureSize) - (reqny / 2);
-            int right = texture.width - 1;
-            int bottom = texture.height - 1;
+            int realX = (int)(textureCoord.x * textureSize) - (splashTexture.width / 2);
+            int realY = (int)(textureCoord.y * textureSize) - (splashTexture.height / 2);
 
-            int x = IntMax(reqX, 0);
-            int y = IntMax(reqY, 0);
-            int nx = IntMin(x + reqnx, right) - x;
-            int ny = IntMin(y + reqny, bottom) - y;
-
-            Color[] pixels = texture.GetPixels(x, y, nx, ny);
-
-            int counter = 0;
-            for (int i = 0; i < nx; ++i)
+            for (int texX = 0; texX < splashTexture.width; texX++)
             {
-                for (int j = 0; j < ny; ++j)
+                for (int texY = 0; texY < splashTexture.height; texY++)
                 {
-                    float currAlpha = splashTexture.GetPixel(i, j).a;
-                    if (currAlpha == 1)
-                        pixels[counter] = targetColor;
-                    else
-                    {
-                        Color currColor = pixels[counter];
-                        // resulting color is an addition of splash texture to the texture based on alpha
-                        Color newColor = Color.Lerp(currColor, targetColor, currAlpha);
-                        // but resulting alpha is a sum of alphas (adding transparent color should not make base color more transparent)
-                        newColor.a = pixels[counter].a + currAlpha;
-                        pixels[counter] = newColor;
-                    }
-                    counter++;
+                    Color result = Color.Lerp(texture.GetPixel(realX + texX, realY + texY), color, splashTexture.GetPixel(texX, texY).a);
+                    result.a += splashTexture.GetPixel(texX, texY).a;
+
+                    texture.SetPixel(realX + texX, realY + texY, result);
                 }
             }
-
-            texture.SetPixels(x, y, nx, ny, pixels);
-            texture.Apply();
         }
-    }
 
-    private int IntMax(int a, int b)
-    {
-        return a > b ? a : b;
-    }
-
-    private int IntMin(int a, int b)
-    {
-        return a < b ? a : b;
+        texture.Apply();
     }
 }
