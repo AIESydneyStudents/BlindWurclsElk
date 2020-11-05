b38492eaee654890fbe391bcdfccc02ad17860cf
Thomas <cnpowerman23@gmail.com>
2020-11-05 14:57:40 +1100

    Add sitting, dialogue, and transition to each scene from carraige
    The player can now sit at each booth, get dialogue, then be transported
    to the respective level. While the text for the dialogue has been added,
    audio still needs to be included.
    
    New collection managers have been made for each level, with a collection
    object script used to enable/disable objects after all items have been
    collected. This is currently used to enable the next booth's interaction
    and disable the current booth.
    
    The collection managers are currently untested.

---
+++ project/Assets/Scripts/Collection Scripts/Collected Objects/CollectionObjects_Japan.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionObjects_Japan : MonoBehaviour
+{
+    [Tooltip("Objects to toggle after all objects in japan have been collected")]
+    public GameObject[] objects;
+
+    // Have all the items been collected?
+    public static bool collected = false;
+
+
+    void Awake()
+    {
+        foreach (var obj in objects)
+        {
+            // Toggle if collected == true
+            obj.SetActive(obj.activeSelf ^ collected);
+        }
+    }
+}
---
+++ project/Assets/Scripts/Collection Scripts/Collection Managers/CollectionManager_Japan.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionManager_Japan : CollectionManagerBase
+{
+    public float fadeTime = 1f;
+    public float delayToShowObject = 1f;
+    public float delayToTransition = 2f;
+
+    public GameObject uiObject;
+
+
+    protected override void AllCollected()
+    {
+        StartCoroutine(ShowObject(delayToShowObject));
+        StartCoroutine(TransitionToTrain(delayToTransition));
+
+        // Enable the collected object in the train carrage
+        CollectionObjects_Japan.collected = true;
+    }
+
+    protected override void NewCollection(int itemID)
+    {
+        // Make the element fade in
+        StartCoroutine(FadeIn(itemID, fadeTime));
+    }
+
+
+    private IEnumerator FadeIn(int itemID, float time)
+    {
+        //used to set color
+        float num = 0;
+        float scale = (1f / time);
+
+
+        while (num != 1f)
+        {
+            num += Time.deltaTime * scale;
+
+            // Change color
+            uiElements[itemID].color = new Color(num, num, num);
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ShowObject(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Hide the UI elements
+        foreach (var elem in uiElements)
+        { elem.gameObject.SetActive(false); }
+        // Show the UI object
+        uiObject.SetActive(true);
+    }
+
+    private IEnumerator TransitionToTrain(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Transition to train with the player sitting in the booth
+        TransitionManager.instance.ChangeScene("TrainCarriage", new Vector3(-0.1f, 1f, 1.1f), true);
+    }
+}
