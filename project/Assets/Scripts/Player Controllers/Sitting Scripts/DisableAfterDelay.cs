cf817c42503023074876585da2d7bc3849a5a05c
Thomas <cnpowerman23@gmail.com>
2020-11-06 09:07:03 +1100

    Fix sitting in train after britain & india
    A script is used to disable the seats not imediatly avalable after a
    delay, allowing the sitting controller to get them from a sphere cast.

---
+++ project/Assets/Scripts/Player Controllers/Sitting Scripts/DisableAfterDelay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAfterDelay : MonoBehaviour
+{
+
+    void Start()
+    {
+        Invoke("Disable", 0.05f);
+    }
+
+    private void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
