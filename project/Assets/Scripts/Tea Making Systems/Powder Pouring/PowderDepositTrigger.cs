30e53b39c9b106b655bc3d93f4f3024669f1d626
Thomas <cnpowerman23@gmail.com>
2020-11-12 14:11:56 +1100

    Add poder pouring minigame
    When the player has the 'spoon' over the powder gather trigger, a bar
    will show, and the player needs to click while the indicator is in the
    'sweet spot'. After this, the spoon needs to enter the powder deposit to
    drop off the powder. After doing this a set number of times, the
    minigame is won (currently a debug log).

---
+++ project/Assets/Scripts/Tea Making Systems/Powder Pouring/PowderDepositTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PowderDepositTrigger : MonoBehaviour
+{
+    // Number of times powder needs to be collected to win
+    public int numNeeded;
+    // Number of times powder has been collected so far
+    int count = 0;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Spoon"))
+        {
+            SpoonScript spoon = other.GetComponent<SpoonScript>();
+
+            if (spoon.hasPowder)
+            {
+                //play anim
+
+                spoon.hasPowder = false;
+                count++;
+
+
+                // If full amount collected, end minigame
+                if (count == numNeeded)
+                {
+                    Debug.Log("win");
+                }
+            }
+        }
+    }
+}
---
+++ project/Assets/Scripts/Tea Making Systems/Powder Pouring/SpoonScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider), typeof(Rigidbody))]
+public class SpoonScript : MonoBehaviour
+{
+    //how high the surface is from 0
+    public float surfHeight;
+    //how high the object should be lifted while being moved
+    public float dragHeight = 1f;
+
+    [HideInInspector]
+    public bool hasPowder = false;
+
+
+
+    // Find the cursor position in world space relative to the object position
+    private Vector3 CursorOnTransform(Vector3 objectPosition)
+    {
+        //find the cursor in world space
+        Vector3 CameraToCursor()
+        {
+            return Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x,
+                Input.mousePosition.y,
+                Camera.main.nearClipPlane)) - Camera.main.transform.position;
+        }
+
+        //transform relative to cam
+        Vector3 camToTrans = objectPosition - Camera.main.transform.position;
+
+        //magic
+        return Camera.main.transform.position +
+            CameraToCursor() *
+            (Vector3.Dot(Camera.main.transform.forward, camToTrans) / Vector3.Dot(Camera.main.transform.forward, CameraToCursor()));
+    }
+
+    void Update()
+    {
+        // Get the cursor pos relative to the object transform
+        Vector3 pos = CursorOnTransform(transform.position);
+        // Set hight to the surface + set drag height
+        pos.y = surfHeight + dragHeight;
+
+        // Set the object's position
+        transform.position = pos;
+
+
+        //temp make it green if has powder
+        if (hasPowder)
+        { GetComponent<MeshRenderer>().material.color = Color.green; }
+        else 
+        { GetComponent<MeshRenderer>().material.color = Color.white; }
+    }
+}
