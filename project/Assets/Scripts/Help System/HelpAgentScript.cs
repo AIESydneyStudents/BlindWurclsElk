cf86b838f1bba97766de2b0d6757b34dacfc3d61
TheTastyGravy <=>
2020-11-18 17:27:05 +1100

    Add sandbox with test agent
    The sandbox level has a navmesh baked and an agent that can use it

---
+++ project/Assets/Scripts/Help System/HelpAgentScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HelpAgentScript : MonoBehaviour
+{
+    public Vector3 targetPos;
+
+    [Tooltip("When the object is this far from the target, it will be destroyed")]
+    public float distance;
+
+
+    void Start()
+    {
+        // Set the AI target
+        GetComponent<NavMeshAgent>().SetDestination(targetPos);
+    }
+
+    void Update()
+    {
+        // When the object gets close enough to the target, destroy it
+        if (Vector3.Distance(transform.position, targetPos) < distance)
+		{
+            Destroy(gameObject);
+		}
+    }
+}
