ebbed331e69fb8cb5edfc6b2fec2e8a10127003b
Thomas <cnpowerman23@gmail.com>
2020-11-19 16:23:17 +1100

    Add pause menu

---
+++ project/Assets/Scripts/Menu Scripts/PauseMenuScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public static bool paused = false;
+
+    public GameObject pauseMenu;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                //hide pause menu
+                pauseMenu.SetActive(false);
+
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+                Time.timeScale = 1;
+                paused = false;
+            }
+            else
+            {
+                //show pause menu
+                pauseMenu.SetActive(true);
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+
+                Time.timeScale = 0;
+                paused = true;
+            }
+        }
+    }
+
+    public void OnResumeClick()
+    {
+        //hide pause menu
+        pauseMenu.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Time.timeScale = 1;
+        paused = false;
+    }
+
+    public void OnExitClick()
+    {
+        Time.timeScale = 1;
+
+        //show the main menu
+    }
+}
