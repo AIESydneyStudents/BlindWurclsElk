c36ed627eb06764d1c83faf0f01f64dd50d32b07
Thomas <cnpowerman23@gmail.com>
2020-11-20 09:15:08 +1100

    Bug #152 Able to interact with tea master twice
    Instead of disabling the object after 2 seconds, now the interaction
    script is destroyed after interacting with it. This allows the remaining
    components to be run and the sit script to be used without being able to
    interact again.

---
+++ project/Assets/Scripts/Interaction System/Components/DisableInteractionComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableInteractionComponent : TriggerComponentBase
+{
+    public override void Activate()
+    {
+        // Get the interaction script
+        InteractionBase script = GetComponent<InteractionBase>();
+
+        // Stop highlighting and destroy the script
+        script.SetHighlight(false);
+        Destroy(script);
+    }
+}
