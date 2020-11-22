ce9da50fe6e517f5e5a0a6a78ea05a0fea92d406
TheTastyGravy <=>
2020-11-22 16:51:29 +1100

    Add tea pouring minigame
    The 5th minigame involves pouring tea from the bowl into 3 tea cups.
    It functions in the same way as water pouring, but with 3 places to pour
    instead one 1.
    
    The minigame lacks proper implementation, with it currently being the
    only minigame avalable when interacting with the booth.

---
+++ project/Assets/Scripts/Tea Making Systems/TeaPouring/TeaCupScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeaCupScript : MonoBehaviour
+{
+    public Transform teaFullPos;
+
+    public GameObject teaObj;
+
+
+    public void RaiseTeaLevel()
+	{
+		StartCoroutine(RaiseTea());
+	}
+
+    private IEnumerator RaiseTea()
+	{
+        //show tea
+        teaObj.SetActive(true);
+
+        float time = 0;
+        float scale = 1f / 3f;
+
+        Vector3 startPos = teaObj.transform.position;
+        Vector3 startScale = teaObj.transform.localScale;
+        //teaObjFull for end
+
+        while (time < 3)
+        {
+            //lerp pos and scale
+            teaObj.transform.position = Vector3.Lerp(startPos, teaFullPos.position, time * scale);
+            teaObj.transform.localScale = Vector3.Lerp(startScale, teaFullPos.localScale, time * scale);
+
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
