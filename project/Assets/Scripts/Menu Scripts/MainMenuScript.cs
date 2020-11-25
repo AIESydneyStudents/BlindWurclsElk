9c6ca7572556e2e434c5370b4d5684c8b8cc0aff
Thomas <cnpowerman23@gmail.com>
2020-11-26 09:47:00 +1100

    Fix new game collectables
    When starting a new game, the static variables for the collection object
    scripts are reset to false.

--- project/Assets/Scripts/Menu Scripts/MainMenuScript.cs
+++ project/Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -9,6 +9,11 @@ public class MainMenuScript : MonoBehaviour
 
     public void OnPlayClick()
     {
+        // Reset collections for new game
+        CollectionObjects_British.collected = false;
+        CollectionObjects_India.collected = false;
+        CollectionObjects_Japan.collected = false;
+
         // Loading the player scene will cause it to load the train scene by default
         SceneManager.LoadScene("PlayerScene");
     }
