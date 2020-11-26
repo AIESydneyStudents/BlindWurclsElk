86a3fd0018da9a749c6944ac0941de68339020a7
Thomas <cnpowerman23@gmail.com>
2020-11-27 09:14:15 +1100

    Trigger looping animation on japan interaction
    Using a new trigger, an animator can be enabled, starting its
    animation

---
+++ project/Assets/Scripts/Interaction System/Components/StartAnimationComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimationComponent : TriggerComponentBase
+{
+    public Animator anim;
+
+
+    public override void Activate()
+    {
+        anim.enabled = true;
+    }
+}
